c4fcf3b4e544ce992ad9c531cd4f05bbd4f8bec9
Joalbert <joalbertgonzalez@gmail.com>
2020-06-27 13:08:23 -0500

    [ADD]🚀 history push and replace added

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 //import './styles.css';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    history.push('/')
+    //history.replace('/')
+  }
+
   return(
-    <>
-      <h1>Login Screen</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
       <hr />
-    </>
+
+      <button
+        className="btn btn-primary"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+    </div>
   );
 }
