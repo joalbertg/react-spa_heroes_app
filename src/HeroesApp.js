321a9cbd26dc8f096a019a3661b6a043517444f0
Joalbert <joalbertgonzalez@gmail.com>
2020-06-27 10:01:48 -0500

    first commit 🚀

---
+++ src/HeroesApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//import './styles.css';
+
+const HeroesApp = () => {
+  return(
+    <>
+      <h1>HeroesApp</h1>
+      <hr />
+    </>
+  );
+}
+
+export default HeroesApp;
+
