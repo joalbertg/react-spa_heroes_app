23021e30870ae41a8aceaa8c75a5b10878b1cd68
Joalbert <joalbertgonzalez@gmail.com>
2020-06-28 00:00:55 -0500

    [ADD]🚀 PrivateRoute component added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboadRoutes } from '../routers/DashboardRoutes';
 
 export const AppRouter = () => {
+  const { user: { logged } } = useContext(AuthContext);
   return(
     <Router>
       <div>
         <Switch>
           <Route exact path='/login' component={LoginScreen} />
-          <Route path='/' component={DashboadRoutes} />
+          <PrivateRoute
+            isAuthenticated={logged}
+            path='/'
+            component={DashboadRoutes}
+          />
         </Switch>
       </div>
     </Router>
